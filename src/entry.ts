637b32d717b8b4a523e6c50470bf3da4900a93da
slps20425 <eason_s@hotmail.com>
2026-01-08 23:36:11 +0800

    chore: update firebase auth domain

--- src/entry.ts
+++ src/entry.ts
@@ -6,7 +6,7 @@ declare var firebase: any;
 // Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDMjzdCgNbI9W8pUd6AJoGRQlYDqKNcf_c",
-    authDomain: "wisecat-8df8d.firebaseapp.com",
+    authDomain: "auth.wise-catty.cc",
     projectId: "wisecat-8df8d",
     storageBucket: "wisecat-8df8d.firebasestorage.app",
     messagingSenderId: "1078479155773",
