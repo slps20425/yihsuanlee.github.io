739cc2f51853aa982a628ec3c4e492b0f1700c33
slps20425 <eason_s@hotmail.com>
2026-01-24 10:52:06 +0800

    Auto-commit before deploy

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // Version checker - displays in console
 const APP_VERSION = '1.0.0'; // Auto-synced from package.json
-const BUILD_TIME = '2026-01-24T02:48:17.946Z';
+const BUILD_TIME = '2026-01-24T02:52:04.060Z';
 
 console.log(
     '%c🐱 WiseCat AI %cv' + APP_VERSION,
